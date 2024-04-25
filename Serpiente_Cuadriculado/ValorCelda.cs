f3faaec087f501acd73dbaaf3b081ee35d9a0865
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-04-25 12:04:53 -0400

    Comienzo del proyecto.

---
+++ Serpiente_Cuadriculado/ValorCelda.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * ESTAS VARIABLES NO SE USARÁN POR EL MOMENTO...
+ * 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+*/
+
+namespace Serpiente_Cuadriculado
+{
+    public enum ValorCelda
+    {
+        // CLASE ALTERNATIVA DEL TABLERO (POR CADA CELDA).
+
+        // POR CADA CELDA SE CONTENDRÁN LOS SIGUIENTES ELEMENTOS:
+
+        Normal,
+        Serpiente,
+        Comida,
+        Afuera
+    }
+}
