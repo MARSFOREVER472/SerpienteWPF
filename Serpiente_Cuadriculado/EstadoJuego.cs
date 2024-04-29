ed2162e49826f6b40b316d2e8b9756d5912e2c33
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-04-29 10:59:18 -0400

    Se crearon nuevas clases de apoyo al proyecto.

---
+++ Serpiente_Cuadriculado/EstadoJuego.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serpiente_Cuadriculado
+{
+    public class EstadoJuego
+    {
+        // CLASE A MODO DE REFERENCIA DEL JUEGO...
+
+        public int Filas { get; } // FILAS.
+        public int Columnas { get; } // COLUMNAS.
+        public ValorCelda[,] Celdas { get; } // CELDAS DEL TABLERO.
+        public DireccionesSerpiente Direccion { get; private set; } // DIRECCIONES DE LA SERPIENTE DENTRO DEL TABLERO DEL JUEGO.
+        public int Puntuacion { get; private set; } // PUNTUACIÓN DEL JUEGO.
+        public bool FinDelJuego { get; private set; } // FIN DEL JUEGO.
+        private readonly LinkedList<Posicion> posicionesSerpiente = new LinkedList<Posicion>(); // POSICIONES DE LA SERPIENTE.
+        private readonly Random aleatorio = new Random(); // VALOR ALEATORIO.
+
+        // CONSTRUCTOR DE ESTA CLASE...
+
+        public EstadoJuego(int fila, int columna)
+        {
+            Filas = fila; // FILAS.
+            Columnas = columna; // COLUMNAS.
+            Celdas = new ValorCelda[fila, columna]; // CELDAS.
+            Direccion = DireccionesSerpiente.Derecha; // DIRECCION PREDETERMINADA
+        }
+    }
+}
---
+++ Serpiente_Cuadriculado/Posicion.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serpiente_Cuadriculado
+{
+    internal class Posicion
+    {
+        // AQUÍ SE POSICIONARÁ LA SERPIENTE EN CUALQUIER PARTE DEL TABLERO MEDIANTE ALGUNAS VARIABLES A DECLARAR...
+
+        public int Fila { get; } // POSICIÓN DE LA SERPIENTE EN FILAS.
+        public int Columna { get; } // POSICIÓN DE LA SERPIENTE EN COLUMNAS.
+
+        // AQUÍ SE CREA UN CONSTRUCTOR DE LA CLASE (Posicion)...
+
+        public Posicion(int fila, int columna)
+        {
+            Fila = fila; // FILA DEL TABLERO.
+            Columna = columna; // COLUMNA DEL TABLERO.
+        }
+
+        // FINALMENTE, LA SERPIENTE SE TRASLADA POR CUALQUIER POSICIÓN DENTRO DEL TABLERO...
+
+        public Posicion Traslado(DireccionesSerpiente direccion) // AQUÍ SIMPLEMENTE DEVUELVE UN VALOR INGRESADO CUANDO SE TRASLADA LAS POSICIONES DE TODA LA SERPIENTE DENTRO DEL TABLERO...
+        {
+            return new Posicion(Fila + direccion.DesplazamientoFilas, Columna + direccion.DesplazamientoColumnas);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Posicion posicion &&
+                   Fila == posicion.Fila &&
+                   Columna == posicion.Columna;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Fila, Columna);
+        }
+
+        public static bool operator ==(Posicion left, Posicion right)
+        {
+            return EqualityComparer<Posicion>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Posicion left, Posicion right)
+        {
+            return !(left == right);
+        }
+    }
+}
